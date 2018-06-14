08c0084c96316fede8d57c3159fcfe6210d8e9e4
EC2 Default User <ec2-user@ip-172-31-17-112.us-east-2.compute.internal>
2018-06-14 08:09:53 +0000

    post

--- microposts/app/User.php
+++ microposts/app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
+
 {
     use Notifiable;
 
@@ -26,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
 }
+
+
